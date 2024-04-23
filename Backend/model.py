3a8214bf859b9ba816c85e9b1b0cfeed9cdf2c7a
Paulpwho <paulpwho@gmail.com>
2024-04-23 13:45:05 +0100

    added database
    added database

---
+++ Backend/model.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from database import db
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
