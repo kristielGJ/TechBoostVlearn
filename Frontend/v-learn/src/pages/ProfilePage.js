f3496f839b3f659cb5904bb759d5f2448e83f281
Ajay Dayalani <dayalani.ajay@gmail.com>
2024-04-25 15:34:39 +0100

    stashing changes

--- Frontend/v-learn/src/pages/ProfilePage.js
+++ Frontend/v-learn/src/pages/ProfilePage.js
@@ -4,7 +4,7 @@ import SkillsInfo from "../components/SkillsInfo"
 
 const ProfilePage = () => {
     return (
-        <div className="bg-logo h-screen w screen py-10 px-10">
+        <div className="bg-logo w screen  py-10 px-10">
             <div className="grid grid-cols-2 space-x-5">
             <div className=" flex flex-col space-y-5">
                 <ProfileInfo />
